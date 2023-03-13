6a2ce0521664f157f29ff8342c7ce13931770a4f
Benjamin Toh <toh.benjamin123@gmail.com>
2023-03-14 00:02:01 +0800

    Add JUnit tests

---
+++ src/test/java/seedu/penus/ParserTest.java
@@ -0,0 +1,175 @@
+package seedu.penus;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.penus.modules.ModuleList;
+import seedu.penus.parser.CommandParser;
+import seedu.penus.exceptions.InvalidCommandException;
+import seedu.penus.exceptions.InvalidFormatException;
+import seedu.penus.exceptions.InvalidGradeException;
+import seedu.penus.exceptions.InvalidModuleException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ParserTest {
+    
+    private CommandParser parser;
+    private ModuleList list;
+
+    @BeforeEach
+    public void setUp() {
+        list = new ModuleList();
+        parser = new CommandParser(list);
+    }
+
+    //parse in taken module with correct format
+    @Test
+    void parseTaken_expectCodeYearSemAndGrade() 
+            throws InvalidCommandException, InvalidModuleException, InvalidFormatException, InvalidGradeException {
+        String inputString = "taken CS2113 y/1 s/1 g/A+";
+        String[] input = inputString.split(" ", 2);
+        parser.parseCommand(input);
+        String code = list.getModule(0).getCode();
+        Integer year = list.getModule(0).getYear();
+        Integer sem = list.getModule(0).getSem();
+        String grade = list.getModule(0).getGrade();
+        
+        assertEquals("CS2113", code);
+        assertEquals(1, year);
+        assertEquals(1, sem);
+        assertEquals("A+", grade);
+
+    }
+
+    //parse in plan module with correct format
+    @Test
+    void parsePlan_expectCodeYearAndSem() 
+            throws InvalidCommandException, InvalidModuleException, InvalidFormatException, InvalidGradeException {
+        String inputString = "plan CS2113 y/4 s/2";
+        String[] input = inputString.split(" ", 2);
+        parser.parseCommand(input);
+        String code = list.getModule(0).getCode();
+        Integer year = list.getModule(0).getYear();
+        Integer sem = list.getModule(0).getSem();
+        
+        assertEquals("CS2113", code);
+        assertEquals(4, year);
+        assertEquals(2, sem);
+    }
+
+    //parse in remove module with module found
+    @Test
+    void parseRemove_moduleFound_expectTrue() 
+            throws InvalidCommandException, InvalidModuleException, InvalidFormatException, InvalidGradeException {
+        //add a module to the list and removes it, checks if module list is empty
+        String inputString = "plan CS2113 y/4 s/2";
+        String[] input = inputString.split(" ", 2);
+        parser.parseCommand(input);
+        assertEquals(1, list.size());
+
+        String inputStringRemove = "remove CS2113";
+        String[] inputRemove = inputStringRemove.split(" ", 2);
+        parser.parseCommand(inputRemove);
+        assertEquals(0, list.size());
+    }
+
+    //parse in remove module with no such module in list
+    @Test
+    void parseRemove_noModuleFound_expectException() {
+        String inputString = "remove CS2113";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidCommandException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in an invalid command expecting an InvalidCommandException
+    @Test
+    void parseInvalidCommand_expectException() {
+        String inputString = "hello";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidCommandException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in only the command without code, expects InvalidModuleException
+    @Test
+    void parsePlan_noCode_expectException() {
+        String inputString = "plan";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidModuleException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in plan module missing y/ or s/, expects InvalidFormatException
+    @Test
+    void parsePlan_noYearSemester_expectException() {
+        String inputString = "plan CS2113";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in taken module missing g/, expects InvalidFormatException
+    @Test
+    void parseTaken_noGrade_expectException() {
+        String inputString = "taken CS2113 y/1 s/2";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in plan module with g/ included, expected exception
+    @Test
+    void parsePlan_withGrade_expectException() {
+        String inputString = "plan CS2113 y/1 s/2 g/A+";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in empty y/ or s/ or g/, expected exception
+    @Test
+    void parseTaken_emptyYearSemGrade_expectException() {
+        String inputString = "taken CS2113 y/ s/";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in g/ in unexpected range (A+ to D, S/U)
+    @Test
+    void parseTaken_wrongGrade_expectException() {
+        String inputString = "taken CS2113 y/1 s/2 g/D-";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+
+    //parse in s/ in unexpected range (1 or 2)
+    @Test
+    void parseTaken_wrongSem_expectException() {
+        String inputString = "taken CS2113 y/1 s/4";
+        String[] input = inputString.split(" ", 2);
+        assertThrows(
+            InvalidFormatException.class,
+            () -> parser.parseCommand(input)
+        );
+    }
+}
---
+++ src/test/java/seedu/penus/UiTest.java
@@ -0,0 +1,96 @@
+package seedu.penus;
+
+import org.junit.jupiter.api.Test;
+import seedu.penus.ui.Ui;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UiTest {
+    public static final String WINDOWS_NEWLINE = "\r\n";
+    public static final String IOS_NEWLINE = "\n";
+
+    @Test
+    void testWelcome() {
+        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOutput));
+
+        Ui.printWelcome();
+        String expectedOutput;
+        String os = System.getProperty("os.name");
+
+        if (os.contains("Windows")) {
+            expectedOutput = 
+                    "\t____________________________________" + WINDOWS_NEWLINE +
+                    "\t___   ____  _      _     __  " + WINDOWS_NEWLINE +
+                    "\t| |_) | |_  | |\\ | | | | ( (`" + WINDOWS_NEWLINE +
+                    "\t|_|   |_|__ |_| \\| \\_\\_/ _)_)" + WINDOWS_NEWLINE +
+                    "\tWelcome to PENUS!" + WINDOWS_NEWLINE + 
+                    "\tWhat can I do for you?" + WINDOWS_NEWLINE + 
+                    "\t____________________________________" + WINDOWS_NEWLINE;
+        } else {
+            expectedOutput = 
+                    "\t____________________________________" + IOS_NEWLINE +
+                    "\t___   ____  _      _     __  " + IOS_NEWLINE +
+                    "\t| |_) | |_  | |\\ | | | | ( (`" + IOS_NEWLINE +
+                    "\t|_|   |_|__ |_| \\| \\_\\_/ _)_)" + IOS_NEWLINE +
+                    "\tWelcome to PENUS!" + IOS_NEWLINE + 
+                    "\tWhat can I do for you?" + IOS_NEWLINE + 
+                    "\t____________________________________" + IOS_NEWLINE;
+        }
+        assertEquals(expectedOutput, actualOutput.toString());
+    }
+
+    @Test
+    void testExit() {
+        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOutput));
+
+        Ui.printExit();
+        String expectedOutput;
+        String os = System.getProperty("os.name");
+
+        if (os.contains("Windows")) {
+            expectedOutput = 
+                    "\t____________________________________" + WINDOWS_NEWLINE +
+                    "\t\tBye see you again!" + WINDOWS_NEWLINE + 
+                    "\t____________________________________" + WINDOWS_NEWLINE;
+        } else {
+            expectedOutput = 
+                    "\t____________________________________" + IOS_NEWLINE +
+                    "\t\tBye see you again!" + IOS_NEWLINE + 
+                    "\t____________________________________" + IOS_NEWLINE;
+        }
+        assertEquals(expectedOutput, actualOutput.toString());
+    }
+
+
+    @Test
+    void testMessage() {
+        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actualOutput));
+
+        String expectedOutput;
+        String[] messageInput = {"Hi", "Added correctly", "\tCS2113"};
+        Ui.printMessage(messageInput);
+        String os = System.getProperty("os.name");
+
+        if (os.contains("Windows")) {
+            expectedOutput = 
+                    "\t____________________________________" + WINDOWS_NEWLINE +
+                    "Hi" + WINDOWS_NEWLINE + 
+                    "Added correctly" + WINDOWS_NEWLINE +
+                    "\tCS2113" + WINDOWS_NEWLINE +
+                    "\t____________________________________" + WINDOWS_NEWLINE;
+        } else {
+            expectedOutput = 
+                    "\t____________________________________" + IOS_NEWLINE +
+                    "Hi" + IOS_NEWLINE  + 
+                    "Added correctly" + IOS_NEWLINE  +
+                    "\tCS2113" + IOS_NEWLINE  +
+                    "\t____________________________________" + IOS_NEWLINE;
+        }
+        assertEquals(expectedOutput, actualOutput.toString());
+    }
+}
