c55a7b6a97819de6f692b853a1dbceaa3c7f483c
TayJiunYuan <tayjy2000@gmail.com>
2023-03-28 13:56:01 +0800

    Added JUnit for Init Command

--- src/test/java/seedu/penus/logic/commands/InitCommandTest.java
+++ src/test/java/seedu/penus/logic/commands/InitCommandTest.java
@@ -2,23 +2,33 @@ package seedu.penus.logic.commands;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import seedu.penus.common.exceptions.InvalidCommandException;
+import seedu.penus.common.exceptions.PenusException;
 import seedu.penus.model.ModelManager;
 import seedu.penus.model.User;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
 public class InitCommandTest {
-    private User user = new User();
-
-
-    private ModelManager model = new ModelManager(user, new ArrayList<>(), new ArrayList<>(), new HashMap<>());
+    private final ModelManager modelManager = new ModelManager(new User(), new ArrayList<>(),
+                                                         new ArrayList<>(), new HashMap<>());
+    private InitCommand initCommand;
 
     @Test
-    public void executeValidInitCommand(){
-        new InitCommand("Bob", 4);
-        assertEquals("Bob", model.getUserName());
-        assertEquals("Computer Engineering", model.getUserCourse());
+    public void testInitCommandSuccess() throws Exception {
+        initCommand = new InitCommand("John Doe", 1);
+        CommandResult commandResult = initCommand.execute(modelManager);
+        assertEquals("John Doe", modelManager.getUserName());
+        assertEquals("Biomedical Engineering", modelManager.getUserCourse());
     }
 
+    @Test
+    public void execute_invalidCourseCode_throwsInvalidCommandException() {
+        initCommand = new InitCommand("John Doe", 10);
+        PenusException invalidCommandException = assertThrows(InvalidCommandException.class, () -> initCommand.execute(modelManager));
+        assertEquals("Error: Enter within the index. Please initialize again", invalidCommandException.getMessage());
+    }
 }
