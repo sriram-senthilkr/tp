2727c831a5792693c09850217426891018055ec0
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-03-30 23:10:48 +0800

    Add Details Sequence Diagram to DG, reformat 'help' command

--- src/test/java/seedu/penus/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/penus/logic/commands/HelpCommandTest.java
@@ -17,21 +17,25 @@ public class HelpCommandTest {
                                                          new ArrayList<>(), new HashMap<>());
 
         CommandResult expected = new CommandResult(
-            "exit" 
-                + "\t\t\t\t\t\t\t\tExits the program\n" +
-            "\tlist [FILTER]" 
-                + "\t\t\t\t\t\t\tDisplays a list of all modules taken or planned in the specified Year or Semester\n"
-                + "\t\t\t\t\t\t\t\t\tIf [FILTER] is not specified, then all modules will shown.\n" +
-            "\tmark [MODULE CODE] g/[GRADE]" 
-                + "\t\t\t\t\tMarks the module that has been cleared, while updating its grades\n" +
-            "\tplan [MODULE CODE] y/[YEAR] s/[SEMESTER]" 
-                + "\t\t\tAdds a module to the planner as an untaken module\n" +
-            "\tremove [MODULECODE]" 
-                + "\t\t\t\t\t\tRemoves a module from the planner\n" + 
-            "\tstatus" 
-                + "\t\t\t\t\t\t\t\tDisplays the status of Core Modules and MCs taken\n" +
+            "exit"
+                + "\t\t\t\t\t\t\t\t\t\t\t\t\tExits the program\n" +
+            "\tlist [FILTER]"
+                + "\t\t\t\t\t\t\t\t\t\t\tDisplays a list of all modules taken or planned in the specified Year or Semester\n"
+                + "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tIf [FILTER] is not specified, then all modules will shown.\n" +
+            "\tmark [MODULE CODE] g/[GRADE]"
+                + "\t\t\t\t\t\t\tMarks the module that has been cleared, while updating its grades\n" +
+            "\tplan [MODULE CODE] y/[YEAR] s/[SEMESTER]"
+                + "\t\t\t\tAdds a module to the planner as an untaken module\n" +
+            "\tremove [MODULECODE]"
+                + "\t\t\t\t\t\t\t\t\t\tRemoves a module from the planner\n" +
+            "\tstatus"
+                + "\t\t\t\t\t\t\t\t\t\t\t\t\tDisplays the status of Core Modules and MCs taken\n" +
+            "\tdetails [MODULECODE]"
+                + "\t\t\t\t\t\t\t\t\tDisplays the details of given module, including Title, Description, " +
+                    "Prerequisites, Module Credits \n" +
+                    "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tand if it can be SU-ed.\n" +
             "\ttaken [MODULE CODE] y/[YEAR] s/[SEMESTER] g/[GRADE]"
-                + "\t\tAdds a module to the planner as a module you have already taken", 
+                + "\t\tAdds a module to the planner as a module you have already taken",
             false
         );
         CommandResult actual = new HelpCommand().execute(model);
