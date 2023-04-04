1b69b85f2e09cd91872e8afa03b5521a16e03572
chiayuxuan <chiayuxuan1909@gmail.com>
2023-04-05 00:12:40 +0800

    Edit HelpCommandTest

--- src/test/java/seedu/penus/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/penus/logic/commands/HelpCommandTest.java
@@ -17,41 +17,48 @@ public class HelpCommandTest {
                                                          new ArrayList<>(), new HashMap<>());
 
         CommandResult expected = new CommandResult(
-            "clear [FILTER]" + "\t\t\t\t\t\t\tClears modules in the specified Year or Semester.\n"
-                + "\t\t\t\t\t\t\t\t\tIf [FILTER] is not specified, then all modules will cleared.\n" +
-            "\texit" 
-                + "\t\t\t\t\t\t\t\tExits the program\n" +
-            "\tlist [FILTER]" 
-                + "\t\t\t\t\t\t\tDisplays a list of all modules taken or planned in the specified Year or Semester\n"
-                + "\t\t\t\t\t\t\t\t\tIf [FILTER] is not specified, then all modules will shown.\n" +
-            "\tmark [MODULE CODE] g/[GRADE]" 
-                + "\t\t\t\t\tMarks the module that has been cleared, while updating its grades\n" +
-            "\tplan [MODULE CODE] y/[YEAR] s/[SEMESTER]" 
-                + "\t\t\tAdds a module to the planner as an untaken module\n" +
-            "\tremove [MODULECODE]" 
-                + "\t\t\t\t\t\tRemoves a module from the planner\n" + 
-            "\tstatus" 
-                + "\t\t\t\t\t\t\t\tDisplays the status of Core Modules and MCs taken\n" +
-            "\ttaken [MODULE CODE] y/[YEAR] s/[SEMESTER] g/[GRADE]"
-                + "\t\tAdds a module to the planner as a module you have already taken\n" +
-            "\tdetails [MODULE CODE]" 
-                + "\t\t\t\t\t\tDisplays the details of given module, including Title, Description, " 
-                + "Prerequisites, Module Credits \n"
-                +  "\t\t\t\t\t\t\t\t\tand if it can be SU-ed.\n" +
-            "\tinit n/[NAME] c/[COURSE NUMBER]"
-                + "\t\t\t\t\tInitialize User.\n"
-                + "\t\t\t\t\t\t\t\t\t [COURSE NUMBER] -> [COURSE NAME]\n"
-                + "\t\t\t\t\t\t\t\t\t 1 -> Biomedical Engineering\n"
-                + "\t\t\t\t\t\t\t\t\t 2 -> Chemical Engineering\n"
-                + "\t\t\t\t\t\t\t\t\t 3 -> Civil Engineering\n" 
-                + "\t\t\t\t\t\t\t\t\t 4 -> Computer Engineering\n"
-                + "\t\t\t\t\t\t\t\t\t 5 -> Electrical Engineering\n" 
-                + "\t\t\t\t\t\t\t\t\t 6 -> Environmental Engineering\n" 
-                + "\t\t\t\t\t\t\t\t\t 7 -> Industrial and Systems Engineering\n" 
-                + "\t\t\t\t\t\t\t\t\t 8 -> Mechanical Engineering\n"
-            , 
-            false
-        );
+                ("\n\t" + String.format("%-52s %-97s", "Command", "Description") +
+                        "\n\t" + String.format("%-52s %-97s", "-------", "-----------") +
+                        "\n\t" + String.format("%-52s %-97s",
+                        "clear [FILTER]", "Clears modules in the specified Year or Semester.") +
+                        "\n\t" + String.format("%-52s %-97s", "",
+                        "If [FILTER] is not specified, then all modules will cleared.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s", "exit", "Exits the program.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s", "list [FILTER]",
+                        "Displays a list of all modules taken or planned ") +
+                        "\n\t" + String.format("%-52s %-97s", "", "in the specified Year or Semester.") +
+                        "\n\t" + String.format("%-52s %-97s", "",
+                        "If [FILTER] is not specified, then all modules will shown.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s", "mark [MODULE CODE] g/[GRADE]",
+                        "Marks the module that has been cleared, while updating its grades.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s", "plan [MODULE CODE] y/[YEAR] s/[SEMESTER]",
+                        "Adds a module to the planner as an untaken module.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s",
+                        "remove [MODULECODE]", "Removes a module from the planner.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s",
+                        "status", "Displays the status of Core Modules and MCs taken.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s",
+                        "taken [MODULE CODE] y/[YEAR] s/[SEMESTER] g/[GRADE]",
+                        "Adds a module to the planner as a module you have already taken.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s", "details [MODULE CODE]",
+                        "Displays the details of given module, including") +
+                        "\n\t" + String.format("%-52s %-97s", "",
+                        "Title, Description, Prerequisites, Module Credits") +
+                        "\n\t" + String.format("%-52s %-97s", "", "and if it can be SU-ed.") +
+                        "\n\n\n\t" + String.format("%-52s %-97s",
+                        "init n/[NAME] c/[COURSE NUMBER]", "Initialize User.") +
+                        "\n" +
+                        "\n\t" + String.format("%-52s %-97s", "", "[COURSE NUMBER] -> [COURSE NAME]") +
+                        "\n\t" + String.format("%-52s %-97s", "", "1 -> Biomedical Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "2 -> Chemical Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "3 -> Civil Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "4 -> Computer Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "5 -> Electrical Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "6 -> Environmental Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "7 -> Industrial and Systems Engineering") +
+                        "\n\t" + String.format("%-52s %-97s", "", "8 -> Mechanical Engineering") +
+                        "\n\t"
+                ), false);
         CommandResult actual = new HelpCommand().execute(model);
         assertEquals(expected.feedbackToUser, actual.feedbackToUser);
     }
