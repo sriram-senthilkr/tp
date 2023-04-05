214c5fecd6bed24fad8d7b28d0a3ace429d9ba8e
chiayuxuan <chiayuxuan1909@gmail.com>
2023-04-05 08:46:30 +0800

    changed format of some lines in HelpCommand

--- src/main/java/seedu/penus/logic/commands/HelpCommand.java
+++ src/main/java/seedu/penus/logic/commands/HelpCommand.java
@@ -14,29 +14,30 @@ public class HelpCommand extends Command {
                         "clear [FILTER]", "Clears modules in the specified Year or Semester.") +
                         "\n\t" + String.format("%-52s %s", "",
                         "If [FILTER] is not specified, then all modules will cleared.") +
-                        "\n\n\n\t" + String.format("%-52s %s", "exit", "Exits the program.") +
-                        "\n\n\n\t" + String.format("%-52s %s", "list [FILTER]",
+                        "\n\n\t" + String.format("%-52s %s", "exit", "Exits the program.") +
+                        "\n\n\t" + String.format("%-52s %s", "list [FILTER]",
                         "Displays a list of all modules taken or planned") +
                         "\n\t" + String.format("%-52s %s", "", "in the specified Year or Semester.") +
                         "\n\t" + String.format("%-52s %s", "",
                         "If [FILTER] is not specified, then all modules will shown.") +
-                        "\n\n\n\t" + String.format("%-52s %s", "mark [MODULE CODE] g/[GRADE]",
+                        "\n\n\t" + String.format("%-52s %s", "mark [MODULE CODE] g/[GRADE]",
                         "Marks the module that has been cleared, while updating its grades.") +
-                        "\n\n\n\t" + String.format("%-52s %s", "plan [MODULE CODE] y/[YEAR] s/[SEMESTER]",
-                        "Adds a module to the planner as an untaken module.") +
-                        "\n\n\n\t" + String.format("%-52s %s",
+                        String.format("%n %n \t") +
+                        String.format("%-52s %s", "plan [MODULE CODE] y/[YEAR] s/[SEMESTER]",
+                                "Adds a module to the planner as an untaken module.") +
+                        "\n\n\t" + String.format("%-52s %s",
                         "remove [MODULECODE]", "Removes a module from the planner.") +
-                        "\n\n\n\t" + String.format("%-52s %s",
+                        "\n\n\t" + String.format("%-52s %s",
                         "status", "Displays the status of Core Modules and MCs taken.") +
-                        "\n\n\n\t" + String.format("%-52s %s",
+                        "\n\n\t" + String.format("%-52s %s",
                         "taken [MODULE CODE] y/[YEAR] s/[SEMESTER] g/[GRADE]",
                         "Adds a module to the planner as a module you have already taken.") +
-                        "\n\n\n\t" + String.format("%-52s %s", "details [MODULE CODE]",
+                        "\n\n\t" + String.format("%-52s %s", "details [MODULE CODE]",
                         "Displays the details of given module, including") +
                         "\n\t" + String.format("%-52s %s", "",
                         "Title, Description, Prerequisites, Module Credits") +
                         "\n\t" + String.format("%-52s %s", "", "and if it can be SU-ed.") +
-                        "\n\n\n\t" + String.format("%-52s %s",
+                        "\n\n\t" + String.format("%-52s %s",
                         "init n/[NAME] c/[COURSE NUMBER]", "Initialize User.") +
                         "\n\n\t" + String.format("%-52s %s", "", "[COURSE NUMBER] -> [COURSE NAME]") +
                         "\n\t" + String.format("%-52s %s", "", "1 -> Biomedical Engineering") +
