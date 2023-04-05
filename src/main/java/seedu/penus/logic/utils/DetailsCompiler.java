1f1fdf3ae2b02e37adac2ca7fb380bee533596f3
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-04-05 22:28:35 +0800

    Solved checkstyle error

--- src/main/java/seedu/penus/logic/utils/DetailsCompiler.java
+++ src/main/java/seedu/penus/logic/utils/DetailsCompiler.java
@@ -4,23 +4,6 @@ public class DetailsCompiler extends ModuleRetriever{
     protected String moduleCode;
 
     public static String getDetails(String module) {
-//        try {
-//            String title = getTitle2223(module);
-//            String description = "\t" + getDescription(module);
-//            String prereqs = "\tPre-Requisites: " + getPrerequisite(module);
-//            String credits = "\tMCs: " + getModuleCredit2223(module);
-//
-//            boolean suStatus = getSUstatus(module);
-//            String suStatusDescription;
-//            if (suStatus) {
-//                suStatusDescription = "\tModule can be SU-ed.";
-//            } else {
-//                suStatusDescription = "\tModule cannot be SU-ed.";
-//            }
-//            return title + "\n" + description + "\n" + prereqs + "\n" + credits + "\n" + suStatusDescription;
-//        } catch (Exception e) {
-//            return ": This information is not available";
-//        }
 
         String title;
         String description;
