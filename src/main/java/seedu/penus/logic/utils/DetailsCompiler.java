3581947f72c0d2cb447a03c0d7d2cf00e8727620
Sriram <88694646+sriram-senthilkr@users.noreply.github.com>
2023-04-05 22:24:49 +0800

    Add individual detail checker

--- src/main/java/seedu/penus/logic/utils/DetailsCompiler.java
+++ src/main/java/seedu/penus/logic/utils/DetailsCompiler.java
@@ -4,22 +4,62 @@ public class DetailsCompiler extends ModuleRetriever{
     protected String moduleCode;
 
     public static String getDetails(String module) {
-        try {
-            String title = getTitle2223(module);
-            String description = "\t" + getDescription(module);
-            String prereqs = "\tPre-Requisites: " + getPrerequisite(module);
-            String credits = "\tMCs: " + getModuleCredit2223(module);
+//        try {
+//            String title = getTitle2223(module);
+//            String description = "\t" + getDescription(module);
+//            String prereqs = "\tPre-Requisites: " + getPrerequisite(module);
+//            String credits = "\tMCs: " + getModuleCredit2223(module);
+//
+//            boolean suStatus = getSUstatus(module);
+//            String suStatusDescription;
+//            if (suStatus) {
+//                suStatusDescription = "\tModule can be SU-ed.";
+//            } else {
+//                suStatusDescription = "\tModule cannot be SU-ed.";
+//            }
+//            return title + "\n" + description + "\n" + prereqs + "\n" + credits + "\n" + suStatusDescription;
+//        } catch (Exception e) {
+//            return ": This information is not available";
+//        }
 
+        String title;
+        String description;
+        String prereqs;
+        String credits;
+        String suStatusDescription;
+
+        try {
+            title = getTitle2223(module);
+        } catch (Exception e) {
+            title = "\tTitle is not available";
+        }
+        try {
+            description = "\t" + getDescription(module);
+        } catch (Exception e) {
+            description = "\tDescription is not available";
+        }
+        try {
+            prereqs = "\tPre-Requisites: " + getPrerequisite(module);
+        } catch (Exception e) {
+            prereqs = "\tPre-Requisites information is not available";
+        }
+        try {
+            credits = "\tMCs: " + getModuleCredit2223(module);
+        } catch (Exception e) {
+            credits = "\tModular Credits information is not available";
+        }
+        try {
             boolean suStatus = getSUstatus(module);
-            String suStatusDescription;
             if (suStatus) {
                 suStatusDescription = "\tModule can be SU-ed.";
             } else {
                 suStatusDescription = "\tModule cannot be SU-ed.";
             }
-            return title + "\n" + description + "\n" + prereqs + "\n" + credits + "\n" + suStatusDescription;
         } catch (Exception e) {
-            return ": This information is not available";
+            suStatusDescription = "\tSU information is not available";
         }
+
+        return title + "\n" + description + "\n" + prereqs + "\n" + credits + "\n" + suStatusDescription;
+
     }
 }
