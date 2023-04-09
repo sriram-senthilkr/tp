c02f6cb5cb0853a5602cf5a7e9b18c4ee0d1e877
Ansenn <ansenn.lee@gmail.com>
2023-04-09 15:35:03 +0800

    Edit DetailsCompilerTest

--- src/test/java/seedu/penus/logic/utils/DetailsCompilerTest.java
+++ src/test/java/seedu/penus/logic/utils/DetailsCompilerTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class DetailsCompilerTest {
 
     private DetailsCompiler details;
+
     @Test
     void testGetDetailsForCS2113() {
         String moduleCode = "CS2113";
@@ -22,6 +23,7 @@ class DetailsCompilerTest {
 
         assertEquals(expectedOutput, actualOutput);
     }
+
     @Test
     void testGetDetailsForInvalidModule() {
         String moduleCode = "INVALID MODULE";
@@ -40,33 +42,12 @@ class DetailsCompilerTest {
         assertEquals(expectedOutput, actualOutput);
     }
 
-    //Test ES2631, exists in 22/23 but not 21/22
+    // Test ES2631, exists in 22/23 but not 21/22
     @Test
     void testGetDetailsForNewModule() {
         String moduleCode = "ES2631";
         String actualOutput = details.getDetails(moduleCode);
         String expectedOutput = ": This information is not available";
-
-        assertEquals(expectedOutput, actualOutput);
-    }
-
-    // Test ES2531, exists in 21/22 but not 22/23
-    @Test
-    void testGetDetailsForDiscontinuedModule() {
-        String moduleCode = "ES2531";
-        String actualOutput = details.getDetails(moduleCode);
-        String expectedOutput = "Critical Thinking And Writing\n\tThe challenges of a global " +
-                "engineer increasingly call upon engineers to think critically and communicate effectively " +
-                "to undertake developmental leadership. This course aims to develop and practise students’ " +
-                "critical thinking and writing skills through analysing case studies in engineering leadership, " +
-                "constructing complex engineering-related problems and solutions, presenting arguments " +
-                "effectively and reflecting on personal leadership development. Relevance to engineering practice " +
-                "is emphasized with references to grounded theories of engineering leadership and the seven " +
-                "missing basics of engineering education.\n\tPre-Requisites: 1. Students who are required " +
-                "to take ES1000 Foundation Academic English and/or ES1103 English for Academic Purposes must " +
-                "pass the modules before they are allowed to read this module. \n2. Students who matriculated " +
-                "in AY2014/15 and AY2015/16 are to read the cross-listed modules, GEK1549 and GET1021, " +
-                "respectively.\n\tMCs: 4\n\tModule can be SU-ed.";
 
         assertEquals(expectedOutput, actualOutput);
     }
